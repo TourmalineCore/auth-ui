f73b6ebe6e872cdf86787e1ff828c1e669918398
Yasnovsky <andrey5.1997@mail.ru>
2023-01-20 09:46:18 +0500

    feat: #861m7xzjm: add pages for create and reset password

---
+++ src/features/create/CreatePasswordPage.tsx
@@ -0,0 +1,129 @@
+import {
+  useState, ChangeEvent, FormEvent,
+} from 'react';
+
+import { Input } from '@tourmalinecore/react-tc-ui-kit';
+
+import { clsx } from 'clsx';
+import { useSearchParams } from 'react-router-dom';
+import { api } from '../../common/api';
+import LoginForm from '../../components/LoginForm/LoginForm';
+import { useValidation } from '../../common/hooks/useValidation';
+import Tooltip from '../../components/Tooltip/Tooltip';
+
+import { setLogin } from '../../common/authService';
+import { useAuthenticated } from '../../common/hooks/useAuthenticared';
+
+function CreatePasswordPage() {
+  useAuthenticated();
+
+  const [formData, setFormData] = useState('');
+
+  const [triedToSubmit, setTriedToSubmit] = useState(false);
+  const [isTooltip, setIsTooltip] = useState(false);
+
+  const [searchParams] = useSearchParams();
+
+  const {
+    minLenght,
+    isContainsNumber,
+    isContainsUppercaseLetter,
+    isValid,
+  } = useValidation(formData, {
+    minLenght: 8,
+    isContainsNumber: true,
+    isContainsUppercaseLetter: true,
+  });
+
+  const login = searchParams.get('login');
+  const userResetPasswordToken = searchParams.get('userResetPasswordToken');
+
+  return (
+    <div className="create-password-page">
+      <LoginForm
+        onSubmit={handleFormSubmit}
+        buttonText="Done"
+        buttonDisabled={isValid}
+      >
+
+        <h1 className="create-password-page__title">Create password</h1>
+
+        <div className="create-password-page__login">
+          Логин:
+          {' '}
+          {login || ''}
+        </div>
+        <div style={{
+          position: 'relative',
+        }}
+        >
+          <Input
+            id="password"
+            type="password"
+            label="Create password"
+            className="create-password-page__input ddd"
+            value={formData}
+            isInvalid={!formData && triedToSubmit}
+            validationMessages={['Поле должно быть заполнено']}
+            isMessagesAbsolute
+            onChange={(event: ChangeEvent<HTMLInputElement>) => setFormData(event.target.value)}
+            onFocus={() => setIsTooltip(true)}
+            onBlur={() => setIsTooltip(false)}
+            autoComplete="off"
+          />
+
+          {(isTooltip || formData) && isValid && (
+            <Tooltip
+              className="create-password-page__tooltip"
+            >
+              <ul className="create-password-page__required-list">
+                <li
+                  className={clsx({
+                    'test-e': minLenght,
+                  })}
+                >
+                  больше 8
+                </li>
+                <li className={clsx({
+                  'test-e': isContainsUppercaseLetter,
+                })}
+                >
+                  с большой буквы
+                </li>
+                <li className={clsx({
+                  'test-e': isContainsNumber,
+                })}
+                >
+                  с цифрой
+                </li>
+              </ul>
+            </Tooltip>
+          )}
+        </div>
+
+      </LoginForm>
+    </div>
+  );
+
+  async function handleFormSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    setTriedToSubmit(true);
+
+    if (formData) {
+      try {
+        await api.post('/auth/create-password', {
+          login,
+          userResetPasswordToken,
+          password: formData,
+        });
+
+        await setLogin({ login, password: formData });
+      } catch (e) {
+        setFormData('');
+      }
+    }
+  }
+}
+
+export default CreatePasswordPage;
