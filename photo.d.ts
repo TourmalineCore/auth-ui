ba72956dca471baa668bb17b4bdb7c4e2f463e6e
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2025-10-16 16:49:16 +0500

    refactor: move photo types

---
+++ photo.d.ts
@@ -0,0 +1,11 @@
+declare module '*.svg' {
+
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>
+  const src: string
+  export default src 
+}
+
+declare module '*.png' {
+  const value: any;
+  export default value;
+}
