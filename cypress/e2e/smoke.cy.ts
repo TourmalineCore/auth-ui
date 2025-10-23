0b62112b2165992fedae6a29334bdc203c9e477e
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2025-10-23 10:30:48 +0500

    refactor: refactor test

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -10,47 +10,61 @@ describe(`Auth Flow E2E`, () => {
   WHEN visits the logout page
   SHOULD be redirected back to the Auth page
   `, () => {
-    cy.visit(`/auth`)
+    cy
+      .visit(`/auth`)
 
-    cy.getByData(`login-input`)
+    cy
+      .getByData(`login-input`)
       .type(`wrong@tourmalinecore.com`)
 
-    cy.getByData(`password-input`)
+    cy
+      .getByData(`password-input`)
       .type(`wrongpassword`)
 
-    cy.getByData(`submit-button`)
+    cy
+      .getByData(`submit-button`)
       .click()
 
-    cy.contains(`Error: Invalid email or password`)
+    cy
+      .contains(`Error: Invalid email or password`)
       .should(`be.visible`)
 
-    cy.getByData(`login-input`)
+    cy
+      .getByData(`login-input`)
       .clear()
       .type(Cypress.env(`USER_LOGIN`))
 
-    cy.getByData(`password-input`)
+    cy
+      .getByData(`password-input`)
       .type(`wrongpassword`)
 
-    cy.getByData(`submit-button`)
+    cy
+      .getByData(`submit-button`)
       .click()
 
-    cy.contains(`Error: Invalid email or password`)
+    cy
+      .contains(`Error: Invalid email or password`)
       .should(`be.visible`)
     
-    cy.getByData(`password-input`)
+    cy  
+      .getByData(`password-input`)
       .clear()
       .type(Cypress.env(`USER_PASSWORD`))
 
-    cy.getByData(`submit-button`)
+    cy
+      .getByData(`submit-button`)
       .click()
 
-    cy.window()
+    cy
+      .window()
       .its(`location.href`)
       .should(`not.include`, `/auth`)
 
-    cy.visit(`/auth/logout`)
+    cy
+      .visit(`/auth/logout`)
 
-    cy.url()
+    cy
+      .url()
       .should(`include`, `/auth`)
   })
 })
