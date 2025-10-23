ebafa726f854f22704091abf59af4a812604496f
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2025-10-23 09:53:31 +0500

    feat: add cypress folder

---
+++ cypress/support/component.ts
@@ -0,0 +1,24 @@
+import './commands'
+import '../env-config'
+
+/// <reference types="cypress" />
+
+// styles
+import 'react-datepicker/dist/react-datepicker.css'
+
+import '../../src/styles/index.scss'
+
+// commands
+import { mount } from 'cypress/react18'
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable {
+      mount: typeof mount,
+      compareSnapshot(name: string, options?: any): Chainable<Element>,
+    }
+  }
+}
+
+Cypress.Commands.add(`mount`, mount)
