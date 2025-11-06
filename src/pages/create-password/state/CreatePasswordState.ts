a7f943e63e34a715a58f856d81f14d52ea909625
Ekaterina <echekina@gmail.com>
2025-11-06 18:12:07 +0500

    feat: add init CreatePassword state and getters

---
+++ src/pages/create-password/state/CreatePasswordState.ts
@@ -0,0 +1,20 @@
+import { makeAutoObservable } from 'mobx'
+
+export class CreatePasswordState {
+  private _password = ``
+  private _isTooltipVisible = false
+
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  get password() {
+    return this._password
+  }
+
+  get isTooltipVisible() {
+    return this._isTooltipVisible
+  }
+
+  
+}
