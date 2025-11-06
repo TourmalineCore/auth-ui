e73949c20bedd9475eb237060191d1db6cad6a4e
Ekaterina <echekina@gmail.com>
2025-11-06 18:16:50 +0500

    test: add tests for password management

--- src/pages/create-password/state/CreatePasswordState.cy.ts
+++ src/pages/create-password/state/CreatePasswordState.cy.ts
@@ -4,6 +4,7 @@ import { CreatePasswordState } from './CreatePasswordState'
 
 describe(`CreatePasswordState`, () => {
   describe(`Initialization`, initializationTests)
+  describe(`Password Management`, passwordManagementTests)
 })
 
 function initializationTests() {
@@ -25,5 +26,38 @@ function initializationTests() {
       .to
       .be
       .false
+  })
+}
+
+function passwordManagementTests() {
+  let createPasswordState: CreatePasswordState
+
+  beforeEach(() => {
+    createPasswordState = new CreatePasswordState()
+  })
+
+  it(`
+  GIVEN empty password
+  WHEN setPassword is called with new value
+  SHOULD update password
+  `, () => {
+    createPasswordState.setPassword('password')
+
+    expect(createPasswordState.password)
+      .to
+      .equal('password')
+  })
+
+  it(`
+  GIVEN existing password
+  WHEN setPassword is called with a different value
+  SHOULD update password
+  `, () => {
+    createPasswordState.setPassword('password1')
+    createPasswordState.setPassword('password2')
+
+    expect(createPasswordState.password)
+      .to
+      .equal('password2')
   })
 }
