e95d909fef691be615ef53b79013cd820e3de5b7
Ekaterina <echekina@gmail.com>
2025-11-24 13:55:44 +0500

    feat: add setLogin method

--- src/pages/reset/state/ResetState.ts
+++ src/pages/reset/state/ResetState.ts
@@ -15,4 +15,12 @@ export class ResetState {
   get isSuccessful() {
     return this._isSuccessful
   }
+
+  setLogin({
+    newValue,
+  }: {
+    newValue: string,
+  }) {
+    this._login = newValue
+  }
 }
