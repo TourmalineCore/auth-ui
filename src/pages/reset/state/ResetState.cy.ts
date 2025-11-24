99d01d692e83086580d402cc609377376958b707
Ekaterina <echekina@gmail.com>
2025-11-24 13:55:09 +0500

    test: add loginManagementTests for reset state

--- src/pages/reset/state/ResetState.cy.ts
+++ src/pages/reset/state/ResetState.cy.ts
@@ -4,6 +4,7 @@ import { ResetState } from './ResetState'
 
 describe(`ResetState`, () => {
   describe(`Initialization`, initializationTests)
+  describe(`Login Management`, loginManagementTests)
  
 })
 
@@ -29,3 +30,39 @@ function initializationTests() {
       .false
   })
 }
+
+  function loginManagementTests() {
+    let resetState: ResetState
+
+    beforeEach(() => {
+      resetState = new ResetState()
+    })
+
+    it(`
+    GIVEN empty login
+    WHEN setLogin is called with a new value
+    SHOULD update login
+    `, () => {
+      resetState.setLogin({
+        newValue: `test@tc.com`,
+      })
+
+      expect(resetState.login).to.equal('test@tc.com')
+    })
+
+    it(`
+    GIVEN existing login
+    WHEN setLogin is called with a different value
+    SHOULD update login to this value
+    `, () => {
+      resetState.setLogin({
+        newValue: `test@tc.com`,
+      })
+
+      resetState.setLogin({
+        newValue: `test2@tc.com`,
+      })
+
+      expect(resetState.login).to.equal('test2@tc.com')
+    })
+  }
