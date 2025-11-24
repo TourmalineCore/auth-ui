0bd390741249ffbd96d1f90bbdf8e4f1a7fb68e1
Ekaterina <echekina@gmail.com>
2025-11-24 14:02:51 +0500

    test: add successfulStateTests for reset state

--- src/pages/reset/state/ResetState.cy.ts
+++ src/pages/reset/state/ResetState.cy.ts
@@ -5,6 +5,7 @@ import { ResetState } from './ResetState'
 describe(`ResetState`, () => {
   describe(`Initialization`, initializationTests)
   describe(`Login Management`, loginManagementTests)
+  describe(`Successful State Management`, successfulStateTests)
  
 })
 
@@ -31,38 +32,77 @@ function initializationTests() {
   })
 }
 
-  function loginManagementTests() {
-    let resetState: ResetState
+function loginManagementTests() {
+  let resetState: ResetState
 
-    beforeEach(() => {
-      resetState = new ResetState()
+  beforeEach(() => {
+    resetState = new ResetState()
+  })
+
+  it(`
+  GIVEN empty login
+  WHEN setLogin is called with a new value
+  SHOULD update login
+  `, () => {
+    resetState.setLogin({
+      newValue: `test@tc.com`,
     })
 
-    it(`
-    GIVEN empty login
-    WHEN setLogin is called with a new value
-    SHOULD update login
-    `, () => {
-      resetState.setLogin({
-        newValue: `test@tc.com`,
-      })
+    expect(resetState.login)
+      .to
+      .equal('test@tc.com')
+  })
 
-      expect(resetState.login).to.equal('test@tc.com')
+  it(`
+  GIVEN existing login
+  WHEN setLogin is called with a different value
+  SHOULD update login to this value
+  `, () => {
+    resetState.setLogin({
+      newValue: `test@tc.com`,
     })
 
-    it(`
-    GIVEN existing login
-    WHEN setLogin is called with a different value
-    SHOULD update login to this value
-    `, () => {
-      resetState.setLogin({
-        newValue: `test@tc.com`,
-      })
-
-      resetState.setLogin({
-        newValue: `test2@tc.com`,
-      })
-
-      expect(resetState.login).to.equal('test2@tc.com')
+    resetState.setLogin({
+      newValue: `test2@tc.com`,
     })
-  }
+
+    expect(resetState.login)
+      .to
+      .equal('test2@tc.com')
+  })
+}
+
+function successfulStateTests() {
+  let resetState: ResetState
+
+  beforeEach(() => {
+    resetState = new ResetState()
+  })
+
+  it(`
+  GIVEN default isSuccessful is false
+  WHEN setIsSuccessful is called
+  SHOULD update isSuccessful to true
+  `, () => {
+    resetState.setIsSuccessful()
+
+    expect(resetState.isSuccessful)
+      .to
+      .be
+      .true
+  })
+
+  it(`
+  GIVEN isSuccessful is true
+  WHEN resetIsSuccessful is called
+  SHOULD update isSuccessful to false
+  `, () => {
+    resetState.setIsSuccessful()
+    resetState.resetIsSuccessful()
+
+    expect(resetState.isSuccessful)
+      .to
+      .be
+      .false
+  })
+}
