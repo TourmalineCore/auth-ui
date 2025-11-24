11812eac9189299dffca8aff7d9c248ccfde433c
Ekaterina <echekina@gmail.com>
2025-11-24 13:36:17 +0500

    feat: add reset state with init values

---
+++ src/pages/reset/state/ResetState.ts
@@ -0,0 +1,18 @@
+import { makeAutoObservable } from 'mobx'
+
+export class ResetState {
+  private _login = ''
+  private _isSuccessful = false
+
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  get login() {
+    return this._login
+  }
+
+  get isSuccessful() {
+    return this._isSuccessful
+  }
+}
